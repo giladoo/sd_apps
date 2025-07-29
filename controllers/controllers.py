3f70ed5dfb3c0f7bb31dc1fc9ff19bd2a3d4825a
homayounfar <homayounfar@msn.com>
2025-07-29 08:21:56 +0330

    update

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -13,7 +13,7 @@ from odoo.addons.portal.controllers.web import Home
 class SdAppsWebsite(Home):
     @http.route('/sd_apps/snippet/apps', type='json', auth='public', website=True, methods=["POST"])
     def get_apps(self, app_id=None):
-        print(f"\n apps\n {app_id} \n user ID: {request.env.user.id}")
+        # print(f"\n apps\n {app_id} \n user ID: {request.env.user.id}")
         apps = request.env['sd_apps.settings'].sudo().get_apps(app_id, request.env.user.id)
         return apps
 
